77afb2a54f168e15915ff5cd8bc59eb416d4819e
Naoya Ito <i.naoya@gmail.com>
2023-01-06 04:43:41 +0900

    fix message

--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,7 @@ import Network.Simple.TCP (HostPreference (HostAny), closeSock, serve)
 main :: IO ()
 main = do
   let port = "6379"
-  putStrLn $ "Redis server listening on port" ++ port
+  putStrLn $ "Redis server listening on port " ++ port
 
   serve HostAny port $ \(socket, address) -> do
     putStrLn $ "successfully connected client: " ++ show address

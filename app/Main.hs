76d9014ce0ddd07a023fbec1a81f2a027f700e17
Naoya Ito <i.naoya@gmail.com>
2023-01-06 04:34:22 +0900

    feat: listening port

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,16 @@
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 module Main (main) where
 
-import Network.Simple.TCP (serve, HostPreference(HostAny), closeSock)
-
+import Network.Simple.TCP (HostPreference (HostAny), closeSock, serve)
 
 main :: IO ()
 main = do
-    -- You can use print statements as follows for debugging, they'll be visible when running tests.
-    putStrLn "Logs from your program will appear here"
+  let port = "6379"
+  putStrLn $ "Redis server listening on port" ++ port
 
-    -- Uncomment this block to pass stage 1
-    -- let port = "6379"
-    -- putStrLn $ "Redis server listening on port " ++ port
-    -- serve HostAny port $ \(socket, address) -> do
-    --     putStrLn $ "successfully connected client: " ++ show address
-    --     closeSock socket
+  serve HostAny port $ \(socket, address) -> do
+    putStrLn $ "successfully connected client: " ++ show address
+    closeSock socket
